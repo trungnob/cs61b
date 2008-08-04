0a35bdb86ed07d5fa58870e859fbccdbda6915ac
trungnob <trungnob@b776a18c-4b53-0410-bd7b-8736a7cac544>
2008-08-04 17:36:37 +0000

    Change little bit on the structure of Block  right now it uses two more classes Coordinate and BlockShape .   The block list inside Board doesn't need to be HashSet since I don't think we gona hashing these blocks . We will hash the board though . I changed it to ArrayList.I really want to use Array since the search  by index on array cost constant time. I attempted to use ID as the index to build the list of the block.But I think it's not worth it since The big Oh of compareToGoal still O(n*m) I wish we could improve this by think of some other way.   Fixed the Goal check because the input not gona be the board. Rather, it is the list of the block.   According to Emin , there's misconception of length and width of the board. We will fixed it later if we encouter problems with it

--- trunk/src/Solver.java
+++ trunk/src/Solver.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.StringTokenizer;
+import java.util.*;
 
 
 public class Solver {
@@ -48,6 +48,52 @@ public class Solver {
 		
 	}
 	
+	
+	public static ArrayList<block> getGoal(String filename) throws IOException{
+         ArrayList<block> result= new ArrayList<block>();
+		BufferedReader initConfig = null;
+		try {
+			initConfig = new BufferedReader (new FileReader (filename));
+		} catch (Exception e) {
+			System.err.println ("Couldn't access file!");
+			System.exit (1);
+		}
+		String line = null;
+//		line = initConfig.readLine();
+		StringTokenizer tokens = new StringTokenizer (" ");
+//		
+		//int row = Integer.parseInt((String) tokens.nextElement ( ));
+		//int col = Integer.parseInt((String) tokens.nextElement ( ));
+		
+		//Board board= new Board(row, col);
+		
+		while ((line=initConfig.readLine()) != null) {
+			tokens = new StringTokenizer (line, " ");
+			int b_len = Integer.parseInt((String) tokens.nextElement ( ));
+			int b_wid = Integer.parseInt((String) tokens.nextElement ( ));
+			int b_row = Integer.parseInt((String) tokens.nextElement ( ));
+			int b_col = Integer.parseInt((String) tokens.nextElement ( ));
+			
+			block b= new block(b_len, b_wid, b_row, b_col);
+			result.add(b);
+			//board.block_set.add((new block(b_len, b_wid, b_row, b_col)));
+			//block b0 = board.adding_block(b_len, b_wid, b_row, b_col);
+			//board.block_set.add(b0);
+			//board.block_set = board.adding_block(b_len, b_wid, b_row, b_col);
+		}
+		
+		System.out.println("Goal has "+ result.size()+" blocks");
+		
+		initConfig.close();
+		
+		return result;
+		
+	}
+	
+	public static boolean isAtGoal(Board playBoard, Board goalBoard){
+		
+		return false;
+	}
 ////////////////////////////////////////////////////////////////////////////////////////
 	
 	
@@ -62,16 +108,16 @@ public class Solver {
 			inputConfigName = args[1];
 			finalConfigName = args[2];
 		} else{
-			System.out.println("Wrong number of arguments");
+			System.out.println("Wrong number of arguments");	
 			System.exit (0);
 		}
 		
-        Board my_board, board_final;
+        Board my_board;//board_final;
         
         my_board = board_setup(inputConfigName);
         System.out.println();
-        board_final = board_setup(finalConfigName);
-       
+       ArrayList<block>theGoal= getGoal(finalConfigName);
+        System.out.println(my_board.compareToGoal(theGoal));
         //if (my_board.equal(board_final) )
         //	System.out.println("the compare is good");
 		
