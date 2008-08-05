1c977a1c00bc550caefc2deba83b74b5e1ddb385
clark0820 <clark0820@b776a18c-4b53-0410-bd7b-8736a7cac544>
2008-08-05 22:12:21 +0000

    so I added hashCode and equals in Board.java and run some simple check, the equals should work (the equals works regardless of how you ID the blocks, as long as the same dimension blocks are in the same position, it will return true)

---
+++ trunk/Solver.java
@@ -0,0 +1,132 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+
+public class Solver {
+
+	
+////////////////////get the initial configuration and print the board //////////////////
+	public static Board board_setup(String filename) throws IOException{
+
+		BufferedReader initConfig = null;
+		try {
+			initConfig = new BufferedReader (new FileReader (filename));
+		} catch (Exception e) {
+			System.err.println ("Couldn't access file!");
+			System.exit (1);
+		}
+		String line = null;
+		line = initConfig.readLine();
+		StringTokenizer tokens = new StringTokenizer (line, " ");
+		
+		int row = Integer.parseInt((String) tokens.nextElement ( ));
+		int col = Integer.parseInt((String) tokens.nextElement ( ));
+		
+		Board board= new Board(row, col);
+		
+		while ((line=initConfig.readLine()) != null) {
+			tokens = new StringTokenizer (line, " ");
+			int b_len = Integer.parseInt((String) tokens.nextElement ( ));
+			int b_wid = Integer.parseInt((String) tokens.nextElement ( ));
+			int b_row = Integer.parseInt((String) tokens.nextElement ( ));
+			int b_col = Integer.parseInt((String) tokens.nextElement ( ));
+			
+			board.adding_block(b_len, b_wid, b_row, b_col);
+		}
+		
+		board.displayBoard();
+		
+		initConfig.close();
+		
+		return board;
+		
+	}
+	
+	
+	public static ArrayList<block> getGoal(String filename) throws IOException{
+         ArrayList<block> result= new ArrayList<block>();
+		BufferedReader initConfig = null;
+		try {
+			initConfig = new BufferedReader (new FileReader (filename));
+		} catch (Exception e) {
+			System.err.println ("Couldn't access file!");
+			System.exit (1);
+		}
+		String line = null;
+
+		StringTokenizer tokens = new StringTokenizer (" ");
+		
+		while ((line=initConfig.readLine()) != null) {
+			tokens = new StringTokenizer (line, " ");
+			int b_len = Integer.parseInt((String) tokens.nextElement ( ));
+			int b_wid = Integer.parseInt((String) tokens.nextElement ( ));
+			int b_row = Integer.parseInt((String) tokens.nextElement ( ));
+			int b_col = Integer.parseInt((String) tokens.nextElement ( ));
+			
+			block b= new block(b_len, b_wid, b_row, b_col);
+			result.add(b);
+		}
+		
+		System.out.println("Goal has "+ result.size()+" blocks");
+		
+		initConfig.close();
+		
+		return result;
+		
+	}
+	
+	public static boolean isAtGoal(Board playBoard, Board goalBoard){
+		
+		return false;
+	}
+////////////////////////////////////////////////////////////////////////////////////////
+	
+	
+	public static void main (String [ ] args)  throws IOException{
+		
+		String inputConfigName = "", finalConfigName = "";
+		
+        if (args.length == 2) {
+        	inputConfigName = args[0];
+        	finalConfigName = args[1];
+		} else if (args.length == 3){
+			inputConfigName = args[1];
+			finalConfigName = args[2];
+		} else{
+			System.out.println("Wrong number of arguments");	
+			System.exit (0);
+		}
+		
+        Board my_board;//board_final;
+        
+        my_board = board_setup(inputConfigName);
+        System.out.println();
+        /*ArrayList<block>theGoal= getGoal(finalConfigName);
+        System.out.println(my_board.compareToGoal(theGoal));
+        block b=my_board.findBlockByID(1);
+        if (b==null) System.out.println("It is null!!\n");else 
+        my_board.MoveRight(b);
+        my_board.displayBoard();
+        b=my_board.findBlockByID(3);
+        my_board.MoveUp(b);
+        System.out.println();
+        my_board.displayBoard();
+        b=my_board.findBlockByID(6);
+        my_board.MoveUp(b);
+        System.out.println();
+        my_board.displayBoard();
+        b=my_board.findBlockByID(2);
+        my_board.MoveLeft(b);
+        System.out.println();
+        my_board.displayBoard();*/
+        Board check_board = board_setup(finalConfigName);
+        System.out.println("checking :" + my_board.equals(check_board));
+        
+        
+        //if (my_board.equal(board_final) )
+        //	System.out.println("the compare is good");
+		
+	}
+}
---
+++ trunk/block.java
@@ -0,0 +1,113 @@
+
+public class block { 
+
+//	private int block_len;
+//	private int block_wid;
+//	private int block_row;
+//	private int block_col;
+	private BlockShape block_shape;
+	private Coordinate block_pos;
+	private int ID; // ID is the identification number of block which help to distinguish two different block even though it have same shape. 
+	public block(){
+//		block_len = 0;
+//		block_wid = 0;
+		block_shape=new BlockShape();
+		block_pos=new Coordinate();
+	}
+	public block(int length, int width, int row, int col){
+//		block_len = length;
+//		block_wid = width;
+//		block_row = row;
+//		block_col = col;
+		block_shape=new BlockShape(length,width);
+		block_pos=new Coordinate(row,col);
+	}
+	
+	public block(int length, int width, int row, int col, int IDnum){
+//		block_len = length;
+//		block_wid = width;
+//		block_row = row;
+//		block_col = col;
+		block_shape=new BlockShape(length,width);
+		block_pos=new Coordinate(row,col);
+    	ID=IDnum;
+	}
+	public int hashCode ( ) { 
+		//return (block_len-1)*256^3 + (block_wid-1) * 256^2 + (block_row) * 256 + (block_col);
+		return 1 ; //---> I don't know yet 
+	}
+	
+	public boolean equals(Object obj){ // two equal blocks only have meaning when they are both on the same board.
+		if ( (this.get_block_row()== ((block)obj).get_block_row()) && (this.get_block_col() == ((block)obj).get_block_col()) 
+			 && (this.get_block_len() == ((block)obj).get_block_len()) && (this.get_block_wid() == ((block)obj).get_block_wid()) )
+			return true;
+		else
+			return false;
+//		return this.ID==((block)obj).ID;
+	}
+	
+	public int getID(){
+		return ID;
+	}
+	
+	public boolean isSameShape(block toCompare){
+		return this.block_shape.equals(toCompare.block_shape);
+	}
+	/*public String toString(){
+	    return 	numtostring(block_len)+numtostring(block_wid)+numtostring(block_row)+numtostring(block_col);
+	}
+	
+	public String numtostring(int num){
+		if (num <10){
+			System.out.println(num);
+			System.out.println("00"+Integer.toString(num));
+			return "00"+Integer.toString(num);}
+		else if (num <100)
+		    return "0"+Integer.toString(num);
+		else
+			return ""+Integer.toString(num);
+	}*/
+	
+	public int get_block_row(){
+		return block_pos.getRow();
+	}
+	
+	public int get_block_col(){
+		return block_pos.getCol();
+	}
+	
+	public int get_block_len(){
+		return block_shape.getLength();
+	}
+	
+	public int get_block_wid(){
+		return block_shape.getWidth();
+	}
+	public void MoveTo(Coordinate To){
+		block_pos.setCol(To.getCol());
+		block_pos.setRow(To.getRow());
+	}
+	public void MoveTo(int Row, int Col){
+		block_pos.setCol(Col);
+		block_pos.setRow(Row);
+	}
+	public void MoveLeft(){
+	MoveTo(block_pos.getRow(),block_pos.getCol()-1);
+	}
+	public void MoveRight(){
+		MoveTo(block_pos.getRow(),block_pos.getCol()+this.get_block_wid());
+		}
+	public void MoveUp(){
+		MoveTo(block_pos.getRow()-1,block_pos.getCol());
+		}
+	public void MoveDown(){
+		MoveTo(block_pos.getRow()+this.get_block_len(),block_pos.getCol());
+		}
+	
+	
+	/*
+	public static void main (String [ ] args){
+		block b0 = new block(10,1,1,1);
+		System.out.println(b0);
+	}*/
+}
